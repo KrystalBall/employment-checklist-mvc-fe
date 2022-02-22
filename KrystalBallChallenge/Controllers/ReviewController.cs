112df9652c53b26cf1018f93d43367adfa229271
Andy Edmonds <edmonds.andrewc@gmail.com>
2022-02-22 09:22:23 -0500

    init

---
+++ KrystalBallChallenge/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KrystalBallChallenge.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
